eac870406a43e2fba9b77f915561fb9eafa13f42
kimilguk <boramcom@daum.net>
2022-10-21 17:07:36 +0900

    ppt01교시 작업소스

---
+++ src/ppt01/Hello.java
@@ -0,0 +1,15 @@
+package ppt01;
+/**
+ * Hello 클래스는 본인 소개를 출력하는 프로그램 입니다.
+ * @author kimilguk
+ *
+ */
+public class Hello {
+
+	public static void main(String[] args) {
+		// 여기에 문장을 추가한다.
+		System.out.println("안녕하세요?");
+		System.out.println("저는 자바를 처음 공부합니다.\n제 이름은 김일국 입니다.");
+	}
+
+}
