f30df0d2c5878122085b0d375a3df48156435156
kimilguk <boramcom@daum.net>
2022-10-22 18:56:44 +0900

    자바02_01교시 작업소스

--- src/ppt02/AreaTest.java
+++ src/ppt02/AreaTest.java
@@ -10,8 +10,8 @@ class CircleArea {//원의 넓이를 구하는 기능의 클래스
 	}
 }
 public class AreaTest {//실행 클래스
-	public static void main(String[] args) {
-		CircleArea CA = new CircleArea(5);
+	public static void main(String[] args) {//아규먼트=Args=매개변수
+		CircleArea CA = new CircleArea(5);//파라미터=인자, 클래스형 변수 CA 객체이다.
 		System.out.println("반지름이 5인 원의 넓이는 = "+ CA.getArea());
 		boolean x = 1 < 2;
 		System.out.println(x);
